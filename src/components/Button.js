9456fa6dfcfae534ebf598ceeb96f4916aa8ea41
Calvin <cabagbyii@gmail.com>
2022-04-01 19:15:57 -0400

    made page responsive

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,7 @@
 import './Button.css';
 
 const Button = ({ className, value }) => {
-	return <div className={className}>{value}</div>
+	return <div className={className} data-name={value}>{value}</div>
 }
 
 export default Button
